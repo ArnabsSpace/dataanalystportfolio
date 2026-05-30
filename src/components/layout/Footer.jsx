4ee587e598051f2bd2b64afdd0d43b99f8d5fa8e
ArnabsSpace <royarnab.ao2@gmail.com>
2026-05-30 14:04:05 +0530

    footter chnages

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <span className={styles.text}>© 2025 Sourabh Banerjee</span>
+      <span className={styles.text}>© 2026 Sourabh Banerjee</span>
       <span className={styles.brand}>Football Tactical &amp; Data Analyst</span>
       <span className={styles.text}>All Rights Reserved</span>
     </footer>
